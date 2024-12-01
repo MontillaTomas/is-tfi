f741ad11dabe7dd433393a8d0715af0bf44ffafd
Tomas Montilla <montilla.tom1@gmail.com>
2024-12-01 17:32:06 -0300

    Implementacion de los DTOs y Mappers para ObraSocial, HistoriaClinica y Paciente

---
+++ src/main/java/com/example/is_tfi/dto/ObraSocialDTO.java
@@ -0,0 +1,12 @@
+package com.example.is_tfi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ObraSocialDTO {
+    private int codigo;
+    private String denominacion;
+    private String sigla;
+}
