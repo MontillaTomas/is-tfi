df03d8af55f389c974e6e1bbeb9f1f9f11cdfe6d
facundo250203 <facundo250203@gmail.com>
2024-12-02 19:57:41 -0300

    Manejo de errores y validaciones

--- src/main/java/com/example/is_tfi/dto/DireccionDTO.java
+++ src/main/java/com/example/is_tfi/dto/DireccionDTO.java
@@ -1,15 +1,28 @@
 package com.example.is_tfi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class DireccionDTO {
+    @NotBlank(message = " Debe ingresar una calle.")
     private String calle;
+
+    @Positive(message = " El numero no puede ser negativo.")
     private int numero;
+
     private int piso;
+
+    @NotBlank(message = " Debe ingresar un departamento.")
     private String departamento;
+
+    @NotBlank(message = " Debe ingresar un código postal.")
     private String codigoPostal;
+
+    @NotBlank(message = "Debe ingresar una ciudad.")
     private String ciudad;
 }
---
+++ src/main/java/com/example/is_tfi/excepciones/RestManejoExcepciones.java
@@ -0,0 +1,70 @@
+package com.example.is_tfi.excepciones;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class RestManejoExcepciones {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> manejarValidaciones(MethodArgumentNotValidException ex, WebRequest request) {
+
+        Map<String, String> errores = new HashMap<>();
+
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+
+            String mensajePersonalizado = "El valor de " + error.getField() + " no es válido: " + error.getDefaultMessage();
+            errores.put(error.getField(), mensajePersonalizado);
+        });
+
+        ErrorMessage errorMessage = new ErrorMessage(
+                HttpStatus.BAD_REQUEST,
+                "Error de validación: revisa los campos proporcionados",
+                null
+        );
+        errorMessage.setDetalles(errores);
+
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PacienteYaPoseeDiagnosticoExcepcion.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<String> pacienteYaPoseeDiagnostico(PacienteYaPoseeDiagnosticoExcepcion excepcion) {
+        return new ResponseEntity<>(excepcion.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(PacienteNoEncontradoExcepcion.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> pacienteNoEncontrado(PacienteNoEncontradoExcepcion excepcion) {
+        return new ResponseEntity<>(excepcion.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DiagnosticoNoEncontradoExcepcion.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> diagnosticoNoEncontrado(DiagnosticoNoEncontradoExcepcion excepcion) {
+        return new ResponseEntity<>(excepcion.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DNIenUsoExcepcion.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<String> dnienUso(DNIenUsoExcepcion excepcion) {
+        return new ResponseEntity<>(excepcion.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(CUILenUsoExcepcion.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<String> cuilenUso(CUILenUsoExcepcion excepcion) {
+        return new ResponseEntity<>(excepcion.getMessage(), HttpStatus.CONFLICT);
+    }
+
+}
