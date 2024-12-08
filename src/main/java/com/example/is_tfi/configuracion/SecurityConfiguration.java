69d7a59e0e518ef957272a83b1829724fd1d8e26
JuanLopezAsis27 <juan.lopezasis27@hotmail.com>
2024-12-08 03:47:39 -0300

    Se modifico configuracion de cors

--- src/main/java/com/example/is_tfi/configuracion/SecurityConfiguration.java
+++ src/main/java/com/example/is_tfi/configuracion/SecurityConfiguration.java
@@ -27,7 +27,10 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf()
+        http
+                .cors()
+                .and()
+                .csrf()
                 .disable()
                 .authorizeHttpRequests()
                 .requestMatchers("api/v1/autenticacion/**")
